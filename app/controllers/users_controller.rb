ab63550f31f0c3716f3b70c0e0620328973eb8ed
katz <info@bridgeutopiaweb.com>
2010-08-24 19:44:08 +0800

    Require user for edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  
+  before_filter :require_user, :only => [:edit, :update]
+    
+    
   def show
     @user = User.find(params[:id])
     @title = @user.name
