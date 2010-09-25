6d696bf57e89cc6adfa9c65bd8e039d7ddd6c375
katz <info@bridgeutopiaweb.com>
2010-09-25 18:46:27 +0800

    Validate uniqueness of twitter status id

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,12 +2,17 @@ class Tweet  < ActiveRecord::Base
   
   validates :twitter_id,  :presence => true
   validates :body, :presence => true
+  validates :twiter_status_id, :uniqueness=>true
     
   require 'hpricot'
   require 'pp'
   
   def self.get_feeds
-    pp Twitter::Search.new('#railsjam').since(100.days.ago).fetch().results
+    begin 
+      pp Twitter::Search.new('#railsjam').since(100.days.ago).fetch().results
+    rescue
+      return nil 
+    end
   end
   
   def self.recent
